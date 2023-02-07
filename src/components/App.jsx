7445de7ec30e919da44e15350d437ca46c69097c
Maxim Lakhman <107705528+lakhmanmaxim@users.noreply.github.com>
2023-02-07 18:46:03 +0200

    task 1, 2, 6 - complete
    контакт с  именем и номером добавляется
    форма (инпуты) очищаются
    контакт удаляется

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,29 +5,51 @@ import styles from './ContactFrom/contactForm.module.css';
 export class App extends Component {
   state = {
     contacts: [
-      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+      // { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
     ],
     name: '',
     number: '',
   };
 
-  addContact = evt => {
-    evt.preventDefaul();
+  addContact = (evt) => {
+    evt.preventDefault();
+    
+    this.setState(prevState => {
+      const { name, number, contacts} = prevState;
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      }
+
+      return {contacts: [newContact, ...contacts], name: "", number: ""}
+    })
   };
 
-  onChange = () => {};
+  deleteContact = (id) => {
+    this.setState(({contacts}) => {
+      const newContact = contacts.filter(contact => contact.id !== id);
+      return {contacts: newContact}
+    })
+  }
+
+  onInputChange = ({target}) => {
+    const {name, value } = target;
+    this.setState({
+      [name]: value,
+    })
+  };
 
   render() {
-    const { contacts } = this.state;
-    const { addContact } = this;
+    const { contacts, name, number } = this.state;
+    const { addContact, onInputChange, deleteContact } = this;
 
     const contact = contacts.map(({ id, name, number }) => (
       <li key={id} className={styles.list_item}>
-        {name}: <span >{number}</span>
-        <button className={styles.contacts_btn}>Delete</button>
+        <p className={styles.item_text}>{name}: </p>
+        <span >{number}</span>
+        <button onClick={() => deleteContact(id)} className={styles.contacts_btn} type="button">Delete</button>
       </li>
     ));
 
@@ -42,11 +64,12 @@ export class App extends Component {
                 Name:
               </label>
               <input
-                onChange={() => {}}
+                onChange={onInputChange}
                 className={styles.contact_input}
                 type="text"
                 placeholder="Enter name"
                 name="name"
+                value={name}
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
@@ -58,11 +81,12 @@ export class App extends Component {
                 Tel.:
               </label>
               <input
-                onChange={() => {}}
+                onChange={onInputChange}
                 className={styles.contact_input}
                 type="tel"
                 placeholder="Enter phone number"
                 name="number"
+                value={number}
                 pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
