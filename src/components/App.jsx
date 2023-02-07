780b382d13c9ab29cfbc27861a4b3a011b648556
Maxim Lakhman <107705528+lakhmanmaxim@users.noreply.github.com>
2023-02-07 21:16:21 +0200

    some change in contacts
    some change in contacts
    add nanoid() for ID

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,8 @@ export class App extends Component {
     contacts: [
       { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
       { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
     name: '',
